092f772dd3d239e101d41f387a26cc3d80bb3f51
maks-buren630501 <maks.bur2809@gmail.com>
2021-05-27 12:13:39 +0300

    [saphir] - изменение метода взаимодействия серверов бек и фронтенда, добавления генерации статических файлов на лету

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,6 +1,9 @@
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import TemplateView
+
+from core import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,6 +12,6 @@ urlpatterns = [
     path('api/reports/', include('official_documents.urls')),
     path('api/objects/', include('objects.urls')),
     re_path('', TemplateView.as_view(template_name='index.html'), name='index'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
