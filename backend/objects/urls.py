501b00ec10d44756e19e47257d6fe1e402e45151
pushkin <maks.bur2809@gmail.com>
2021-06-18 16:34:16 +0300

    [backend.objects] - добавлен url для списка связей

--- backend/objects/urls.py
+++ backend/objects/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     url(r'^list_type', views.aj_object_type_list),
     url(r'^list_classifier', views.aj_list_classifier),
     url(r'^object', views.aj_object),
+    url(r'^reletions', views.aj_list_rels)
 ]
