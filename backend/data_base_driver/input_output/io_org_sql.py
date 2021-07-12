c18a2aa920247a35aa0f517dd81c02e3ec76d8bf
pushkin <maks.bur2809@gmail.com>
2021-07-12 17:04:56 +0300

    [backend.objects] - сильное добавление объекта(игнорирует повторы)

--- backend/data_base_driver/input_output/io_org_sql.py
+++ backend/data_base_driver/input_output/io_org_sql.py
@@ -65,6 +65,7 @@ class IO_ORG_SQL():
             if not data_pars.rec_id:
                 # создать rec_id и вставить ОДНОЙ опрацией без проверки на повторы
                 data_pars.rec_id = self.io_sql.obj_rec_id_new(obj_id=data_pars.obj_id)
+
                 # надо вернуть с учетом обработки после комита
                 # if self.io_sql.obj_insert_row_all(data_pars=data_pars) != len(data_pars.row_dic):
                     # raise Exception('Error insert row: data_pars = ' + str(data_pars))
