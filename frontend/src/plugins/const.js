930f3c230a8f7bdb63f3de2ef96de540ae343740
vg <vgena09@gmail.com>
2021-07-29 15:42:26 +0300

    [frontend.map.edit] map Edit tree

---
+++ frontend/src/plugins/const.js
@@ -0,0 +1,12 @@
+
+export class APP {
+  static COLOR             = '#00796B';
+};
+
+export class TREE {
+  static ICON_SIZE         = '20'; // undefined; // '20';
+  static ICON_FOLDER_CLOSE = 'mdi-folder-outline';
+  static ICON_FOLDER_OPEN  = 'mdi-folder-open-outline';
+  static COLOR_DEFAULT     = undefined; // '#616161';
+  static COLOR_SELECT      = APP.COLOR;
+};
