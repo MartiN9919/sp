9152c71b25b5f09088d6a5fcb715b6b37567150c
pushkin <maks.bur2809@gmail.com>
2021-08-20 14:57:28 +0300

    [backend.data_base_driver.relation] - изменение API запроса

--- backend/objects/urls.py
+++ backend/objects/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^list_classifier', views.aj_list_classifier),
     url(r'^list_type', views.aj_object_type_list),
+    url(r'^object_relation', views.aj_object_relation),
     url(r'^object', views.aj_object),
     url(r'^relations', views.aj_list_rels),
     url(r'^search', views.aj_search_objects),
