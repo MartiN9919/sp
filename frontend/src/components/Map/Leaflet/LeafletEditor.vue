ac0d39e903036dcbcf4218429aa876d5418ea4d3
vg <vgena09@gmail.com>
2021-08-25 16:59:52 +0300

    [backend+frontend.osm] +

--- frontend/src/components/Map/Leaflet/LeafletEditor.vue
+++ frontend/src/components/Map/Leaflet/LeafletEditor.vue
@@ -163,8 +163,9 @@ export default {
     },
     on_nav_add(id, name, fc) {
       console.log(33, id, name, fc);
-      this.fc_child  = JSON.parse(JSON.stringify(fc));
-      this.fc_parent = fc_merge([this.fc_parent, this.fc_child,]);
+      let fc_temp  = JSON.parse(JSON.stringify(fc));
+      this.fc_child = fc_merge([this.fc_child, fc_temp,]);
+      this.fc_parent = this.fc_child;
       this.$refs.notify.notify_add(name);
     },
 
