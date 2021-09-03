c804f00409132c26fad54942206a5adc42a62b39
pushkin <maks.bur2809@gmail.com>
2021-09-03 12:56:04 +0300

    [backend.files] - добавление проверок и логирования на запрос скачивания

--- backend/files/views.py
+++ backend/files/views.py
@@ -5,11 +5,14 @@ from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
 from django.http import JsonResponse, HttpResponse
 
+from core.projectSettings.decoraters import login_check, request_log, request_get
 from core.settings import MEDIA_ROOT
 from data_base_driver.constants.const_dat import DAT_OWNER
 from data_base_driver.input_output.input_output import io_set
 
-
+@login_check
+@request_log
+@request_get
 def aj_download(request):
     path = request.path.split('download')[1]
     path_start_directory = path.split('/')[1]
