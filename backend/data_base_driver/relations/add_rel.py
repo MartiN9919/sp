aec00f1702efe69f0f23a9b424b2d5ce73e92621
pushkin <maks.bur2809@gmail.com>
2021-09-22 12:29:31 +0300

    [backend.object.relations] - изменение возврощаемого типа связи

--- backend/data_base_driver/relations/add_rel.py
+++ backend/data_base_driver/relations/add_rel.py
@@ -3,7 +3,7 @@ import datetime
 from data_base_driver.additional_functions import get_date_time_from_sec
 from data_base_driver.constants.const_dat import DAT_REL
 from data_base_driver.input_output.input_output import io_set, io_get_rel
-from data_base_driver.relations.get_rel import get_rel_cascade
+from data_base_driver.relations.get_rel import get_rel_cascade, get_object_relation
 from data_base_driver.sys_key.get_key_dump import get_key_by_id
 
 
@@ -47,7 +47,8 @@ def add_rel(group_id, object_1_id, rec_1_id, object_2_id, rec_2_id, params, doc_
         else:
             result_list.append({'id': int(temp['key_id']), 'values': [{'val': temp['val'],
                                                                        'date': get_date_time_from_sec(temp['sec'])}]})
-    return result_list
+    return get_object_relation(group_id, object_1_id, rec_1_id, [{'object_id': object_2_id, 'rec_id': rec_2_id}])
+    # return result_list
 
 
 def add_rel_by_other_object(group_id, object_id, rec_id, other_object_id, other_rec_id):
