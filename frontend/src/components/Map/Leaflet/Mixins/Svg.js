eb841495cba1b46d2e9633fd9d4d4c808c748fe8
vg <vgena09@gmail.com>
2021-10-15 10:56:57 +0300

    [frontend.map svg]-

--- frontend/src/components/Map/Leaflet/Mixins/Svg.js
+++ frontend/src/components/Map/Leaflet/Mixins/Svg.js
@@ -1,29 +1,37 @@
+import { MAP_SVG } from '@/components/Map/Leaflet/Mixins/Svg.const';
+
 export default {
   data: () => ({
-    color_temp: 'blue'
+    color_id:   'head-arrow-2',
+    color_temp: 'blue',
+    svg: [],
   }),
+
+  mounted: function() {
+      let el = document.createElement('svg');
+      document.body.prepend(el);
+      el.outerHTML = MAP_SVG.CONST.TXT;
+  },
+
+  beforeDestroy: function() {
+
+  },
+
+  watch: {
+    svg: function(val) {
+
+    },
+  },
+
   methods: {
-    // ВАЖНО
-    // вызывать из родительского mounted или method.onMapReady
+    // вызывать из родительского mounted
     mounted_after_svg() {
       let el = document.createElement('svg');
       document.body.prepend(el);
       el.outerHTML = `
 <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" width="0" height="0">
   <defs>
-    <pattern id="fill-star" x="0" y="0" width="25" height="25" patternUnits="userSpaceOnUse">
-      <circle cx="10" cy="10" r="10"/>
-    </pattern>
-
-    <pattern id="fill-diagonal-hatch" patternUnits="userSpaceOnUse" width="4" height="4">
-      <path d="M-1,1 l2,-2 M0,4 l4,-4 M3,5 l2,-2" style="stroke:black; stroke-width:1;" />
-    </pattern>
-
-    <marker id='head-arrow' orient="auto" markerUnits='userSpaceOnUse' markerWidth='2' markerHeight='4' refX='0.1' refY='0'>
-      <path d='M0,0 V4 L2,2 Z' fill="red"/>
-    </marker>
-
-    <marker id='head-arrow-2' orient="auto" markerUnits='userSpaceOnUse' markerWidth='101' markerHeight='33' refX='0.1' refY='16.5' fill='${this.color_temp}' opacity='.5'>
+    <marker id='${this.color_id}' fill='${this.color_temp}' orient="auto" markerUnits='userSpaceOnUse' markerWidth='101' markerHeight='33' refX='0.1' refY='16.5' opacity='.5'>
       <path d='M1,17 L21,1 L21,33 Z'/>
       <path d='M101,17 L82,1 L82,33 Z'/>
       <rect x="20" y="6" width="64" height="5" />
@@ -36,12 +44,39 @@ export default {
     },
   },
 
+    // <marker id='head-arrow-2' fill='${this.color_temp}' orient="auto" markerUnits='userSpaceOnUse' markerWidth='101' markerHeight='33' refX='0.1' refY='16.5' opacity='.5'>
+    //   <path d='M1,17 L21,1 L21,33 Z'/>
+    //   <path d='M101,17 L82,1 L82,33 Z'/>
+    //   <rect x="20" y="6" width="64" height="5" />
+    //   <rect x="20" y="23" width="64" height="5" />
+    // </marker>
 
-//
-//fill='#555'
-//       <path d='M1,6 L8,1 L8,11 Z' fill="white" stroke="context-stroke"/>
+
+
+  //   <pattern id='head-arrow-1' width='101' height='33'>
+  //     <path d='M1,17 L21,1 L21,33 Z'/>
+  //     <path d='M101,17 L82,1 L82,33 Z'/>
+  //     <rect x="20" y="6" width="64" height="5" />
+  //     <rect x="20" y="23" width="64" height="5" />
+  //   </pattern>
+
+  // </defs>
+
+  // <defs>
+  //   <marker id='head-arrow-2' xlink:href="#head-arrow-1" fill="red" orient="auto" markerUnits='userSpaceOnUse' markerWidth='101' markerHeight='33' refX='0.1' refY='16.5' >
+  //   </marker>
+  // </defs>
+
+// return process.env.BASE_URL+MAP_ITEM.FC.STYLE.MARKER.PATH+name+'.png';
+// import                 '@/components/Map/Leaflet/Markers/Pulse';
+
+// refX='center' 'left', 'top'
+// xlink:href="#head-arrow-1"
+// fill='${this.color_temp}'
+// fill='#555'
 // fill="context-stroke"
-//stroke="context-stroke"
-//stroke:black;
+// <path d='M1,6 L8,1 L8,11 Z' fill="white" stroke="context-stroke"/>
+// stroke="context-stroke"
+// stroke:black;
 
 }
