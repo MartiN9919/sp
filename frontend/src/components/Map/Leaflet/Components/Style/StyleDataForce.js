d951751e6a0e8ed7847043c8205652360e085cd0
vg <vgena09@gmail.com>
2021-11-11 15:03:24 +0300

    [frontend.map Marker SVG helicopter] +

--- frontend/src/components/Map/Leaflet/Components/Style/StyleDataForce.js
+++ frontend/src/components/Map/Leaflet/Components/Style/StyleDataForce.js
@@ -213,6 +213,17 @@ export const ICON = {
       </svg>
     `,
   },
+  // вертолет для траектории
+  'force_helicopter_route_inside': {
+    anchor_dx: 20,
+    anchor_dy: 20,
+    zoom:      .8,
+    svg: `
+      <svg width={width} height={height} viewBox="0 0 40 40">
+        <path style="fill:none;stroke:#f00;stroke-width:4" d="M2,2 38,38 M2,38 38,2"/>
+      </svg>
+    `,
+  },
 }
 
 
@@ -220,6 +231,12 @@ export const ICON = {
 
 
 export const DECOR = {
+  // линия - Х - траектория вертолета прикордон
+  'line-force_helicopter_route_inside': [
+    { offset: '100%', repeat: 0,   symbol_type: 'arrow',  symbol_options: { pixelSize: 8,  pathOptions:   { color: '#f00', weight: 2, stroke: true, }, }, icon_properties: { shadow: false, }, },
+    { offset: 0,      repeat: 150, symbol_type: 'marker', symbol_options: { rotate: true,  markerOptions: { icon: 'icon-svg-force_helicopter_route_inside', }, }, icon_properties: { shadow: false, }, },
+    { offset: 0,      repeat: 20,  symbol_type: 'dash',   symbol_options: { pixelSize: 10, pathOptions:   { color: '#f00', weight: 2, }, }, },
+  ],
 
 }
 
@@ -455,4 +472,26 @@ export const TEST = [
       "coordinates": [28.0,54.0],
     },
   },
+
+
+
+
+  {
+    "type": "Feature",
+    "properties": {
+      "class": "line-force_helicopter_route_inside hidden",
+      "hint": "line-force_helicopter_route_inside hidden",
+      "date": "2021-05-03",
+      //"color": "#888",
+    },
+    "geometry": {
+      "type": "LineString",
+      "coordinates": [
+        [24.0,58.5],
+        [27.5,59.5],
+        [32.0,58.5],
+      ]
+    },
+  },
+
 ]
