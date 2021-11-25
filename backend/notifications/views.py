6a1a730ea4a1d91ee1395861fe59c5e57c40dc76
pushkin <maks.bur2809@gmail.com>
2021-11-25 17:01:55 +0300

    [frontend.map] - испарвление import

--- backend/notifications/views.py
+++ backend/notifications/views.py
@@ -1,7 +1,6 @@
-import json
-
 from core.projectSettings.decorators import login_check, request_log, request_wrap
-from data_base_driver.sys_notifications.get_notifications_info import get_notifications_by_user
+from data_base_driver.sys_notifications.get_notifications_info import get_notifications_by_user, \
+    get_notifications_list_by_offset
 from data_base_driver.sys_notifications.set_notifications_info import set_read
 
 
@@ -29,3 +28,21 @@ def aj_set_read(request, notification_id):
     @return: -
     """
     return set_read(notification_id, request.user.id)
+
+
+@login_check
+@request_log
+@request_wrap
+def aj_notifications_sorted_list(request):
+    """
+    Функция для обработки Get запроса на получение ранжированного списока оповещений
+    @param request: Get запрос
+    @return: json содержащий список оповещений по заданным параметрам и общее их количество
+    """
+    return get_notifications_list_by_offset(request.user.id,
+                                            int(request.GET.get('size', 10)),
+                                            int(request.GET.get('offset', 0)),
+                                            request.GET.get('date'),
+                                            request.GET.get('type'),
+                                            request.GET.get('order')
+                                            )
