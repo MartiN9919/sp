f049d643b5a0f899a8c3db1def42d208d76bddb6
vg <vgena09@gmail.com>
2021-11-29 16:04:37 +0300

    [frontend.map map.viewer] +

---
+++ frontend/src/components/Map/Leaflet/LeafletViewer.vue
@@ -0,0 +1,146 @@
+<template>
+  <div class="zz">
+    <l-map
+      ref="map"
+      style="z-index: 0;"
+      :options="map_options"
+      :crs="MAP_GET_TILES[MAP_GET_TILE].crs"
+      @ready="on_map_ready"
+      @contextmenu=""
+      @dblclick="on_map_dblclick"
+    >
+
+      <!-- ПОДЛОЖКА -->
+      <l-tile-layer
+        :url="MAP_GET_TILES[MAP_GET_TILE].url"
+        :attribution="MAP_GET_TILES[MAP_GET_TILE].attr"
+        :tms="MAP_GET_TILES[MAP_GET_TILE].tms"
+      />
+
+      <!-- РЕДАКТОР -->
+      <EditorMap
+        v-model="fc_child"
+        :modeEnabled="{}"
+        modeSelected=""
+        :modeEdit="false"
+        @resetSelect="on_map_reset_select"
+        @setFocus="on_map_set_focus"
+      />
+
+      <!-- МАСШТАБ -->
+      <l-control-scale
+        v-if="dop_controls"
+        position="bottomright"
+        :imperial="false"
+        :metric="true"
+      />
+
+      <!-- ЛИНЕЙКА -->
+      <l-control-polyline-measure
+        v-if="dop_controls"
+        :options="measure_options()"
+      />
+
+    </l-map>
+  </div>
+</template>
+
+
+
+<script>
+
+import { mapGetters, mapActions, } from 'vuex';
+import 'leaflet';
+import { LMap, LTileLayer, LControlScale, } from 'vue2-leaflet';
+import LControlPolylineMeasure from 'vue2-leaflet-polyline-measure';
+
+import EditorMap    from '@/components/Map/Leaflet/Components/EditorMap';
+import MixResize    from '@/components/Map/Leaflet/Mixins/Resize';
+import MixMeasure   from '@/components/Map/Leaflet/Mixins/Measure';
+
+import { fc_merge } from '@/components/Map/Leaflet/Lib/LibFc';
+
+export default {
+  name: 'LeafletViewer',
+  model: { prop:  ['fc_parent_prop'], event: 'fc_parent_change', },
+  props: {
+    fc_parent_prop: { type: Object,  default: () => undefined, },
+    dop_controls:   { type: Boolean, default: () => true, },  // дополнительные элементы (рулетка, масштаб)
+  },
+
+  components: { LMap, LTileLayer, LControlScale, LControlPolylineMeasure, EditorMap, },
+  mixins: [ MixMeasure, MixResize, ],
+
+  data: () => ({
+    LOCAL_STORAGE_KEY_POSTFIX: 'geometry',
+    fc_child: undefined,
+    map_options: {
+      zoomControl: false,
+      zoomSnap: 0.5,
+    },
+  }),
+
+  created() {
+    this.fc_child = this.fc_parent;
+  },
+
+  mounted() {
+    // установить слушатель map.on_resize
+    this.resize_add(this.$refs.map.$el, this.on_map_resize);
+  },
+
+  watch: {
+    fc_child: function(val) { this.fc_parent = val; },
+  },
+
+  computed: {
+    ...mapGetters([
+      'MAP_GET_TILES',
+      'MAP_GET_TILE',
+    ]),
+
+    fc_parent: {
+      get()    { return this.fc_parent_prop; },
+      set(val) { this.$emit('fc_parent_change', val); },
+    },
+  },
+
+  methods: {
+    ...mapActions([
+      'appendErrorAlert',
+    ]),
+
+    on_map_ready() {
+      this.map = this.$refs.map.mapObject;
+      this.map.doubleClickZoom.disable();
+      this.map.invalidateSize();
+    },
+
+    on_map_resize () {                   // fire from MixResize
+      if (this.map) this.map.invalidateSize();
+    },
+
+    on_map_dblclick(e) {
+      // this.appendErrorAlert({status: 501, content: e.latlng, show_time: 5, });
+    },
+
+    // сбросить выделение (obj, osm): из child.map в свойство child.nav
+    on_map_reset_select() {
+      this.$refs.notify.notify_del();
+    },
+
+    on_map_set_focus() {
+      this.$refs.map.$el.focus()
+    },
+
+  },
+
+}
+</script>
+
+
+
+<style scoped lang="scss">
+  @import "~leaflet/dist/leaflet.css";
+  @import "~@/components/Map/Leaflet/Lib/Lib.css";
+</style>
