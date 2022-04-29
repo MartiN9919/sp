cdbaa992dbf23607b7863604b5b27e2e9ae28c93
vg <vgena09@gmail.com>
2022-04-29 09:26:50 +0300

    [back struct del .old] +

--- .old/back/web/authent/static/js/auth.js
+++
@@ -1,4 +0,0 @@
-// ЛОГОТИП
-let logo         = document.getElementById('logo');
-logo.textContent = logo.textContent.toUpperCase();
-delete logo;
--- .old/back/web/graph/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-
-from dat import views
-
-urlpatterns = [
-    url(r'^', views.main),
-    #url(r'^', views.all),
-]
