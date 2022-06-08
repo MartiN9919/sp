0a2e3a1abd64755debf318b8f4e8c919e850d7f2
pushkin <maks.bur2809@gmail.com>
2022-06-08 10:50:06 +0300

    [backend.core] - исправление бага с константами

--- backend/core/projectSettings/constant.py
+++ backend/core/projectSettings/constant.py
@@ -3,10 +3,10 @@ import os
 
 mode = os.environ.get('MODE')
 
-MEDIA_ROOT = '/devstorage/abiad_data' if mode != 'deploy' else '/deploy_storage/saphir_data'
-DOCUMENT_ROOT = '/devstorage/abiad_documents/' if mode != 'deploy' else '/deploy_storage/saphir_documents/'
-TEMPLATE_ROOT = '/devstorage/abiad_documents/template/' if mode != 'deploy' else '/deploy_storage/saphir_documents/template/'
-LOG_ROOT = '/log/' if mode != 'deploy' else '/deploy_storage/abiad_log/'
+MEDIA_ROOT = '/devstorage/saphir_data' if mode != 'deploy' else '/deploy_storage/saphir_data'
+DOCUMENT_ROOT = '/devstorage/saphir_documents/' if mode != 'deploy' else '/deploy_storage/saphir_documents/'
+TEMPLATE_ROOT = '/devstorage/saphir_documents/template/' if mode != 'deploy' else '/deploy_storage/saphir_documents/template/'
+LOG_ROOT = '/log/' if mode != 'deploy' else '/deploy_storage/log/'
 
 
-DEPLOY_SETTING = json.load(open('/deploy_storage/config_abiad.json')) if mode == 'deploy' else {}
+DEPLOY_SETTING = json.load(open('/deploy_storage/config.json')) if mode == 'deploy' else {}
